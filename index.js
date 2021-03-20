b2103233f33f1cd1aae714c80388d4077ebf70b9
Fernando Toledo <fernando_29837@live.com>
2021-03-20 22:27:45 +0000

    Counting sort function finished.

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const countingSort = (arr) => {
 
 	const range = arr.reduce((acc, val) => acc > val ? acc : val, 0);
 
-	return range;
+	const countArr = Array.from({length: range + 1}, () => 0);
+
+	const outputArr = Array.from({length: n}, () => 0);
+
+
+	for (let i = 0; i < n; i++) {
+		++countArr[arr[i]];
+	}
+
+	for (let i = 1; i <= n; i++) {
+		countArr[i] = countArr[i] + countArr[i - 1];
+	}
+
+	for(let i = n - 1; i >= 0; i--) {
+		outputArr[--countArr[arr[i]]] = arr[i];
+	}
+
+	return outputArr;
 
 }
 
 const arrTest = [6, 4, 7, 2, 9, 4, 5, 1, 8];
 
-countingSort(arrTest);
+const res = countingSort(arrTest);
+
+console.log(res);
